e057d835870b2642cd52f7a5f1c273e5cd015a6d
Ebenezer Don <ebenezerdonugwu@gmail.com>
2019-11-14 10:07:51 +0100

    change image url to https

--- routes/products.js
+++ routes/products.js
@@ -9,49 +9,49 @@ router.get('/', (req, res, next) => {
         id: '1',
         name: 'Denim Pocket Print T-shirt',
         price: 2300,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1552005452/poudb0j6gptbggxztfre.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1552005452/poudb0j6gptbggxztfre.jpg'
       },
       {
         id: 2,
         name: 'Vestidos Vintage Wear',
         price: 5342,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1552004888/imaqrrefbqaq4xa87p4r.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1552004888/imaqrrefbqaq4xa87p4r.jpg'
       },
       {
         id: 3,
         name: 'Block Fleece Hoodie',
         price: 4988,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1552003606/ihuf0noo3hihkmfzk2gn.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1552003606/ihuf0noo3hihkmfzk2gn.jpg'
       },
       {
         id: 4,
         name: 'Jasmine Bell Sleeve Dress',
         price: 5400,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1552002566/noyqvnskvy8kw3zznwmo.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1552002566/noyqvnskvy8kw3zznwmo.jpg'
       },
       {
         id: 5,
         name: "Men's Hoodie Sweat Shirt",
         price: 4020,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1551993378/ky6i07onnyudpe2thlrv.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1551993378/ky6i07onnyudpe2thlrv.jpg'
       },
       {
         id: 6,
         name: 'Short Sleeve T-shirt - White',
         price: 1399,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1551993159/w2jnj2oh47yids0skskl.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1551993159/w2jnj2oh47yids0skskl.jpg'
       },
       {
         id: 7,
         name: 'Long Sleeve Hooded T Shirt',
         price: 2999,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1551992156/hrqinxdgqaxeqwdqbll9.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1551992156/hrqinxdgqaxeqwdqbll9.jpg'
       },
       {
         id: 8,
         name: 'Generic Colourful Face Towels',
         price: 12350,
-        imageUrl: 'http://res.cloudinary.com/learnground/image/upload/v1551992462/cxockdhiymgit9vmb1lo.jpg'
+        imageUrl: 'https://res.cloudinary.com/learnground/image/upload/v1551992462/cxockdhiymgit9vmb1lo.jpg'
       }
     ]
   })
